c645552df00bf62cc792591a9005f7a88fa3253b
Gabriel Falcão <gabriel.falcao@corp.globo.com>
2009-08-21 19:24:13 -0300

    Shouting out loud!

--- test.jquery-shout.js
+++ test.jquery-shout.js
@@ -16,25 +16,42 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-var ShowMessage = function ($self, data) {
-    $self.find(".text").text(data.text);
-
-    var assertionMessage1 = $self.attr('id') +
-                            ' did show the message: "' +
-                            data.text +
-                            '"'
-
+var ShowFromTop = function ($self, data) {
     $self.show('drop', {'direction': 'up'}, 'slow', function (){
-                   ok(true, assertionMessage1);
-                   start();
                    setTimeout(function () {
                                   $self.hide('drop',
                                              {'direction': 'up'},
                                              'slow',
+                                             data.callback || function (){});
+                                  return false;
+                              }, data.timeout || 2000);
+               });
+}
+var ShowFromRight = function ($self, data) {
+    $self.show('drop', {'direction': 'right'}, 'slow', function (){
+                   setTimeout(function () {
+                                  $self.hide('drop',
+                                             {'direction': 'right'},
+                                             'slow',
                                              data.callback);
                                   return false;
-                              }, data.timeout);
+                              }, data.timeout || 2000);
                });
+}
+var ShowMessage = function ($self, data) {
+    if ($self.css("display") != 'none') {
+        $self.hide('drop', {'direction': 'up'}, 'slow', function (){
+                       $self.find(".text").text(data.text);
+                   });
+    } else {
+        $self.find(".text").text(data.text);
+    }
+    var assertionMessage1 = $self.attr('id') +
+                            ' did show the message: "' +
+                            data.text +
+                            '"'
+    ShowFromTop($self, data);
+    ok(true, assertionMessage1);
 }
 var ShoutTests = {
     run: function () {
@@ -100,14 +117,47 @@ var ShoutTests = {
                  ok(typeof $.shout === 'function',
                     'A jQuery should have the method "shout"');
                  // will hearing the event "event-1"
-                 $("#top-message").hear("show-message", ShowMessage);
+                 $("#top-message").hear("show-message", function ($self, data) {
+                                            ShowMessage($self, data);
+                                            start();
+                                        });
                  $.shout("show-message",
                         {text: 'First shout!',
                          timeout: 2000});
                  equals($("#top-message").find(".text").text(),
                         'First shout!',
-                        'After shouting, the top message shout contain the text I specified');
+                        'After shouting, the top message should contain the text I specified');
                  stop();
              });
+       test('Many hearers can take actions at once',
+             function() {
+                 expect(4);
+                 $("#top-message").hear("user-login", function ($self, data){
+                                            var message = 'Hello ' + data.user.name + ', welcome to your webmail!';
+                                            ShowMessage($self, {text: message});
+                                            start();
+                                            ok(true, 'Just shown the message ' + message);
+                                        });
+                 $("#email-user-box").hear("user-login", function ($self, data){
+                                               $self.find(".username").text(data.user.name);
+                                               $self.show('drop', {'direction': 'right'}, 'slow');
+                                               ok(true, 'Just shown the user box for ' + data.user.name);
+                                           });
+                 $("#email-list").hear("user-login", function ($self, data){
+                                           $self.show('drop', {'direction': 'right'}, 'slow');
+                                           ok(true, 'Just shown ' + data.user.name + "'s email list");
+                                       });
+
+                 $.shout("user-login",
+                         {
+                             user: {
+                                 name: 'Gabriel'
+                             },
+                             timeout: 2000
+                         });
+
+                 stop();
+             });
+
     }
 }
